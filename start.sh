53a413f2c2294cbf31454a97c24fdf1bb34e4172
Alias <yygggg@foxmail.com>
2022-11-04 16:17:45 +0800

    update

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
+docker stop starter-django:latest
+docker rm starter-django:latest
 docker build . --file Dockerfile --tag 'starter-django:latest'
-docker run -p 3000:80 -d starter-django:latest
+docker run -p 80:3000 -d starter-django:latest
