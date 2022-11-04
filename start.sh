c462f6114d00e2cfcb316d1d4a4215c16a4573f1
alias <yygggg@foxmail.com>
2022-11-04 17:19:25 +0800

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 docker stop my_django
 docker rm my_django
 docker build . --file Dockerfile --tag 'starter_django:latest'
-docker run -p 1313:8000 -d --name my_django starter_django:latest
+docker run --net host -d --name my_django starter_django:latest
--- starter_django/settings.py
+++ starter_django/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-$j%(dh21=ybm1+cxr@iez6lrzcr*421amqr*%tw-*6o(@%9qs9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
